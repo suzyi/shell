d8a5e40bf4ebb42d7c0b6a2e40ad5635acc7db24
suzyi <1274865991@qq.com>
2022-05-12 10:24:13 +0800

    modify the example of looping over strings

--- loop/loop-over-strings.sh
+++ loop/loop-over-strings.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-for s in "circle-red" "circle-cyan" "circle-yellow";do
-    echo $s
+for file in [ train_list.txt test_list.txt train_lmdb test_lmdb ];do
+    if [ -e ${file} ];then
+        echo deleting ${file}
+        rm -rf ${file}
+    fi
 done
